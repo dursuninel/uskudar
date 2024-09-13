f756b33d34ec468ff1aec003f5ee9c1d6cb836a9
dursuninel <drsinl1905@gmail.com>
2024-09-13 15:52:26 +0300

    .

--- script.js
+++ script.js
@@ -26,3 +26,70 @@ $(".header-nav > ul.navigation-list > li:last-child > a").on(
     $(this).closest("li").toggleClass("active");
   }
 );
+
+var swiper = new Swiper(".banner_main", {
+  effect: "fade",
+  loop: true,
+  autoHeight: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper = new Swiper(".clubs_slide", {
+  // effect: "fade",
+  loop: true,
+  autoHeight: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+// Filtreleme işlemi için tarih hesaplaması
+function calculateDateRange(days) {
+  const today = new Date();
+  const pastDate = new Date(today);
+  pastDate.setDate(today.getDate() - days);
+  return pastDate;
+}
+
+// Tarih filtresine göre karşılaştırma fonksiyonu
+function filterByDate(matchDate, selectedDateFilter) {
+  const matchDateObj = new Date(matchDate.split(".").reverse().join("-"));
+
+  if (selectedDateFilter === "gecen_hafta") {
+    return matchDateObj >= calculateDateRange(7);
+  } else if (selectedDateFilter === "1_ay") {
+    return matchDateObj >= calculateDateRange(30);
+  } else if (selectedDateFilter === "2_ay") {
+    return matchDateObj >= calculateDateRange(60);
+  } else {
+    return true; // Tüm tarihleri göster
+  }
+}
+
+// Filtreleme fonksiyonu
+function filterMatches() {
+  const selectedLeague = $("#leagueFilter").val();
+  const selectedDate = $("#dateFilter").val();
+
+  $(".match_item").each(function () {
+    const matchLeague = $(this).attr("lig");
+    const matchDate = $(this).attr("date");
+
+    // Lig ve tarih filtresine göre görünürlük ayarlama
+    if (
+      (!selectedLeague || matchLeague === selectedLeague) &&
+      filterByDate(matchDate, selectedDate)
+    ) {
+      $(this).css({ display: "flex" });
+    } else {
+      $(this).hide();
+    }
+  });
+}
+
+// Lig ve tarih seçildiğinde filtreleme fonksiyonunu çalıştır
+$("#leagueFilter").on("change", filterMatches);
+$("#dateFilter").on("change", filterMatches);
